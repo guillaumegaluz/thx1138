135cf387c1bd11a248cb4ce580ad172d0ca19cc5
Guillaume Galuz <guillaume.galuz@gmail.com>
2014-03-10 13:31:33 -0700

    Create table and model for movies + migrate db

---
+++ db/migrate/20140310183531_create_table_movies.rb
@@ -0,0 +1,17 @@
+class CreateTableMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.text :title
+      t.integer :release_year
+      t.text :locations
+      t.text :fun_fact
+      t.text :production_company
+      t.text :distributor
+      t.text :director
+      t.text :writer
+      t.text :actor_1
+      t.text :actor_2
+      t.text :actor_3
+    end
+  end
+end
